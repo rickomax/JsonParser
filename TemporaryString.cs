5db5f5997143ca8e8e7440e93629eb600dabaddc
Ricardo Reis <rickomax@hotmail.com>
2023-01-24 13:59:43 -0300

    Add files via upload

---
+++ TemporaryString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace JsonParser
+{
+    public struct TemporaryString
+    {
+        private char[] _chars;
+        private string _charString;
+        private readonly int _length;
+
+        public TemporaryString(int length)
+        {
+            _charString = new string('\0', length);
+            _chars = new char[length];
+            _length = length;
+        }
+
+        public string GetString(JsonParser.JsonValue value)
+        {
+            unsafe
+            {
+                fixed (char* c = _chars)
+                {
+                    UnsafeUtility.MemSet(c, 0, _length * sizeof(char));
+                    value.CopyTo(_chars);
+                    fixed (char* s = _charString)
+                    {
+                        for (var i = 0; i < _length; i++)
+                        {
+                            s[i] = c[i];
+                        }
+                    }
+                }
+            }
+            return _charString;
+        }
+    }
+}
